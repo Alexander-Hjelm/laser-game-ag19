1bbd56eb49ddc447d90b7d660667f1f35b0e58bf
ronya <ronyab@kth.se>
2019-09-02 11:40:36 +0200

    laser + reflection

---
+++ laser-game-ag19/Assets/Laser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{   
+    [SerializeField]
+    Vector3 forward;
+    LineRenderer lineRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+        GetComponent <LineRenderer>();
+        lineRender = GetComponent <LineRenderer>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit raycastHit;
+        bool hit = Physics.Raycast(transform.position, forward, out raycastHit, LayerMask.GetMask("Mirror"));
+        if(hit){
+            Vector3 point = raycastHit.point;
+            Vector3 normal = raycastHit.normal;
+            Vector3 v = point - transform.position;
+            Vector3 r = 2*v - 2*Vector3.Project(v, normal);
+
+            lineRender.SetVertexCount(3);
+            lineRender.SetPosition(0, transform.position); 
+            lineRender.SetPosition(1, point);
+            lineRender.SetPosition(2, r);
+        }
+        
+        
+
+
+        
+    }
+
+
+
+}
