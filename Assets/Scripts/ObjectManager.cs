779d8c867a08d00f1716763c8efff816a27b9105
Kasim Taskin <kaszim@github.com>
2019-09-07 22:31:06 +0200

    Rotation and removal of objects

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -20,6 +20,8 @@ public class ObjectManager : MonoBehaviour
     {
         _gameObjects = new Dictionary<int, long>();
         TUIOInput.OnObjectAdded += OnObjectAdded;
+        TUIOInput.OnObjectUpdated += OnObjectUpdated;
+        TUIOInput.OnObjectRemoved += OnObjectRemoved;
     }
 
     private void OnObjectAdded(object sender, TuioObjectEventArgs e)
@@ -37,13 +39,39 @@ public class ObjectManager : MonoBehaviour
         }
 
         var type = (Objects)e.Object.ClassId;
-
-        _gameObjects.Add(e.Object.Id, GameManager.SpawnPrefab(type.ToString(), new Vector3((e.Object.X - .5f) * 44f, 0, (e.Object.Y - .5f) * 44f), Vector3.forward));
+        var rot = Quaternion.AngleAxis(Mathf.Rad2Deg * e.Object.Angle, Vector3.up);
+        var gameId = GameManager.SpawnPrefab(type.ToString(), ScreenToWorld(e.Object.X, e.Object.Y), rot);
+        _gameObjects.Add(e.Object.Id, gameId);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnObjectUpdated(object sender, TuioObjectEventArgs e)
     {
-        
+        if (!_gameObjects.ContainsKey(e.Object.Id))
+        {
+            //TODO: Perhaps we should just add it instead?
+            Debug.LogError(("Tried to update an object that was not added priorly."));
+            return;
+        }
+        Debug.Log(e.Object.Angle);
+        var gameId = _gameObjects[e.Object.Id];
+        var rot = Quaternion.AngleAxis(Mathf.Rad2Deg * e.Object.Angle, Vector3.up);
+        GameManager.SetPositionOfSpawnedObject(gameId, ScreenToWorld(e.Object.X, e.Object.Y));
+        GameManager.SetRotationOfSpawnedObject(gameId, rot);
+    }
+
+    private void OnObjectRemoved(object sender, TuioObjectEventArgs e)
+    {
+        if (!_gameObjects.ContainsKey(e.Object.Id))
+        {
+            Debug.LogError(("Tried to remove an object that was not added priorly."));
+            return;
+        }
+
+        GameManager.RemoveSpawnedObject(_gameObjects[e.Object.Id]);
+    }
+
+    private Vector3 ScreenToWorld(float x, float y)
+    {
+        return new Vector3((x - .5f) * 44f, 0, (.5f - y) * 44f);
     }
 }
