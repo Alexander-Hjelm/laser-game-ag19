c52b079889d24cdb932d98669a2ddabb43c670b2
Alexander Hjelm <nick.nertova@gmail.com>
2019-09-13 15:06:39 +0200

    Unique materials for each laser hit effect color

--- Assets/Scripts/LaserBounceEffect.cs
+++ Assets/Scripts/LaserBounceEffect.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class LaserBounceEffect : MonoBehaviour
 {
+    private Dictionary<Color, string> _materialPathByColor = new Dictionary<Color, string>()
+    {
+        {Color.red, "Materials/FireBallRed"},
+        {Color.blue, "Materials/FireBallBlue"},
+        {Color.green, "Materials/FireBallGreen"},
+        {Color.magenta, "Materials/FireBallMagenta"},
+        {Color.cyan, "Materials/FireBallCyan"},
+        {Color.yellow, "Materials/FireBallYellow"}
+    };
+
+    private Dictionary<Color, string> _sparksMaterialPathByColor = new Dictionary<Color, string>()
+    {
+        {Color.red, "Materials/SparksRed"},
+        {Color.blue, "Materials/SparksBlue"},
+        {Color.green, "Materials/SparksGreen"},
+        {Color.magenta, "Materials/SparksMagenta"},
+        {Color.cyan, "Materials/SparksCyan"},
+        {Color.yellow, "Materials/SparksYellow"}
+    };
+    
     public void SetColor(Color color)
     {
-        Debug.Log("SetColor");
+        ParticleSystemRenderer particleSystemRenderer = GetComponent<ParticleSystemRenderer>();
+        particleSystemRenderer.material = Resources.Load<Material>(_materialPathByColor[color]);
+
+        ParticleSystemRenderer sparksRenderer = transform.Find("ElectricalSparks").GetComponent<ParticleSystemRenderer>();
+        sparksRenderer.trailMaterial = Resources.Load<Material>(_sparksMaterialPathByColor[color]);
     }
 
 }
