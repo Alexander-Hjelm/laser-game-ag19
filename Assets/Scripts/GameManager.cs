78d048904747f73beb62e16a16a2bbccd131d122
bhatnathan <bhatnathan@gmail.com>
2019-09-25 22:48:25 +0200

    Sounds work

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -184,7 +184,7 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator LoadNextSceneDelayed()
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene(nextLevel);
     }
 
