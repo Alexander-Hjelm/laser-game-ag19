7eca110677dee62457f3bbfc82141405fece9d72
Nathan Bhat <bhatnathan@gmail.com>
2019-09-26 13:46:12 +0200

    Sound fix (#69)
    * Changed Laser sound
    
    * Added door sound
    
    * Added fanfare when beating level
    
    * Sounds work
    
    * Updated door sound

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -109,6 +109,7 @@ public class GameManager : MonoBehaviour
             _spawnedObjectsById.Clear();
             _splitLasersThisFrame.Clear();
             _notifiedLasersThisFrame.Clear();
+            GetComponent<AudioSource>().PlayOneShot(GetComponent<AudioSource> ().clip);
             LevelTransitionAnimation.StartAnimateOut();
             StartCoroutine(LoadNextSceneDelayed());
             _nextLevelLoaded = true;
@@ -183,7 +184,7 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator LoadNextSceneDelayed()
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene(nextLevel);
     }
 
