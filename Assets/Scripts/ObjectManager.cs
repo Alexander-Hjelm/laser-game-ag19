f8b286a015f35bf70d9a839e4e409d66700053ec
Kasim Taskin <kaszim@github.com>
2019-10-08 22:42:47 +0200

    Use two class ids for screen objects

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -1,214 +1,112 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 using TUIOsharp.DataProcessors;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = System.Object;
 
 // Spawnable objects
-public enum Objects
+public enum ObjectType
 {
     Mirror,
     Prism,
-    Laser
+    Laser,
 }
 
 public class ObjectManager : MonoBehaviour
 {
-    
+    // Availability of each Object Type and its corresponding class ids
+    public (ObjectType, int, int)[] ClassAvailability = new (ObjectType, int, int)[]
+    {
+        (ObjectType.Mirror, 0, 1),
+        (ObjectType.Mirror, 2, 3),
+        (ObjectType.Mirror, 4, 5),
+        (ObjectType.Mirror, 6, 7),
+        (ObjectType.Mirror, 8, 9),
+        (ObjectType.Prism, 10, 11),
+        (ObjectType.Prism, 12, 13),
+        (ObjectType.Prism, 14, 15),
+        (ObjectType.Prism, 16, 17),
+        (ObjectType.Laser, 18, 19),
+        (ObjectType.Laser, 20, 21),
+        (ObjectType.Laser, 22, 23),
+        (ObjectType.Laser, 24, 25),
+    };
     [System.Serializable]
-    public class ObjectScreenSize { public Objects type; public Vector2 size; }
-    [System.Serializable]
-    public class MaxObject { public Objects type; public int max; }
+    public class MaxObject { public ObjectType type; public int max; }
 
     [SerializeField()]
     public MaxObject[] MaxObjectsPerType; // List containing limits for each object type
     [SerializeField()]
-    public ObjectScreenSize[] ObjectScreenSizes;
     public GameObject AuraDisplay;
 
-    private class TUIOObject
-    {
-        public int id;
-        public long gameId;
-        public Objects type;
-        public Vector2 screenPosition;
-        public float angle;
-        public Zone zone;
-    }
-
-    private Dictionary<int, TUIOObject> _gameObjects;
-    private Dictionary<int, TUIOObject> _badObjects;
-
-    private Vector4[] _shaderArray;
-    private float[] _shaderAngleArray;
+    // All spawned game objects from screen objects go here
+    // (unique id from classId1 + classId2, GameObject Id, Type)
+    private Dictionary<int, (long, ObjectType)> _gameObjects = new Dictionary<int, (long, ObjectType)>();
 
     private Zone[] zones;
-
     private void Awake()
     {
-        _shaderArray = new Vector4[10];
-        _shaderAngleArray = new float[10];
-        _gameObjects = new Dictionary<int, TUIOObject>();
-        _badObjects = new Dictionary<int, TUIOObject>();
-        TUIOInput.OnObjectAdded += OnObjectAdded;
-        TUIOInput.OnObjectUpdated += OnObjectUpdated;
-        TUIOInput.OnObjectRemoved += OnObjectRemoved;
         zones = FindObjectsOfType<Zone>();
     }
 
     private void Update()
     {
-        var rw = AuraDisplay.GetComponent<RawImage>();
-
-        var arr = _badObjects.Values.Select(go =>
+        var screenObjects = TUIOInput.GetScreenObjects();
+        foreach (var (type, classId1, classId2) in ClassAvailability)
         {
-            var oss = ObjectScreenSizes.FirstOrDefault(o => o.type == go.type);
-            if (oss == null)
+            var uniqueId = classId1 + classId2;
+            // First check if there are two of these in screen objects
+            var screenObject1 = TUIOInput.GetScreenObjects(classId1).FirstOrDefault();
+            var screenObject2 = TUIOInput.GetScreenObjects(classId2).FirstOrDefault();
+            if (screenObject1 != null && screenObject2 != null)
             {
-                return new Vector4(go.screenPosition.x, go.screenPosition.y, 0.1f, 0.1f);
-            }
-            return new Vector4(go.screenPosition.x, go.screenPosition.y, oss.size.x, oss.size.y);
-        }).ToArray();
-        Array.Copy(arr, _shaderArray, arr.Length);
-        var angles = _badObjects.Values.Select(go => go.angle).ToArray();
-        Array.Copy(angles, _shaderAngleArray, angles.Length);
-        rw.material.SetInt("_ObjectsLength", arr.Length);
-        if (arr.Length > 0)
-        {
-            rw.material.SetVectorArray("_Objects", _shaderArray);
-            rw.material.SetFloatArray("_ObjectAngles",_shaderAngleArray);
-        }
-
-        foreach (var go in _gameObjects.Values)
-        {
-            // A proper mod function that turns negative values into positive
-            float mod(float x, float m)
-            {
-                return (x % m + m) % m;
-            }
-
-            var transform = GameManager.GetSpawnedObject(go.gameId).transform;
-            var currAngle = transform.eulerAngles.y;
-            var targetAngle = Mathf.Rad2Deg * go.angle;
-            // Two ways of turning towards that angle
-            var deltaAngle = mod(targetAngle - currAngle, 360);
-            var negativeDeltaAngle = deltaAngle - 360f;
-            
-            // Choose the angle that needs the least amount of rotation
-            deltaAngle = -negativeDeltaAngle < deltaAngle ? negativeDeltaAngle : deltaAngle;
-            // Multiply by constant rotational speed
-            deltaAngle *= 2f;
-            // Add to our current angle
-            currAngle += deltaAngle * Time.deltaTime;
-            var rot = Quaternion.AngleAxis(currAngle, Vector3.up);
-            GameManager.SetRotationOfSpawnedObject(go.gameId, rot);
-        }
-    }
-
-    private void OnObjectAdded(object sender, TuioObjectEventArgs e)
-    {
-        if (_gameObjects.ContainsKey(e.Object.Id))
-        {
-            Debug.LogError("An object input was received for an object that already exists");
-            return;
-        }
-
-        if (!Enum.IsDefined(typeof(Objects), e.Object.ClassId))
-        {
-            Debug.LogError($"Unknown Object class id: {e.Object.ClassId}");
-            return;
-        }
-
-        var type = (Objects) e.Object.ClassId;
-
-        var tuioObj = new TUIOObject()
-        {
-            id = e.Object.Id,
-            type = type,
-            screenPosition = new Vector2(e.Object.X, 1 - e.Object.Y),
-            angle = e.Object.Angle
-        };
-
-        if (MaxObjectsReached(tuioObj.type))
-        {
-            _badObjects.Add(tuioObj.id, tuioObj);
-            return;
-        }
-
-        var (isPlaced, zone) = TryPlace(tuioObj);
-        if (isPlaced)
-        {
-            AddObjectToWorld(tuioObj, zone);
-            tuioObj.zone = zone;
-        }
-        else
-        {
-            _badObjects.Add(tuioObj.id, tuioObj);
-        }
-    }
-
-    private void OnObjectUpdated(object sender, TuioObjectEventArgs e)
-    {
-        if (!_gameObjects.ContainsKey(e.Object.Id))
-        {
-            if (_badObjects.ContainsKey(e.Object.Id))
-            {
-                var badgo = _badObjects[e.Object.Id];
-                badgo.screenPosition = new Vector2(e.Object.X, 1 - e.Object.Y);
-                badgo.angle = e.Object.Angle;
-                // Let's see if the bad object is now a proper one
-                // First check if max objects has been reached
-                if (!MaxObjectsReached(badgo.type))
+                // If we have these two, we either spawn or update a gameobject
+                if (_gameObjects.ContainsKey(uniqueId))
                 {
-                    // Try to place the object
-                    var (isPlaced, zone) = TryPlace(badgo);
-                    if (isPlaced)
+                    // We have already spawned this game object, instead update it
+                    var (gameId,_) = _gameObjects[uniqueId];
+                    var angle = Direction(screenObject1, screenObject2);
+                    var rot = Quaternion.AngleAxis(Mathf.Rad2Deg * angle, Vector3.up);
+                    var pos = ScreenToWorld(Position(screenObject1, screenObject2));
+                    GameManager.SetPositionOfSpawnedObject(gameId, pos);
+                    GameManager.SetRotationOfSpawnedObject(gameId, rot);
+                    // If we get removed from a zone simply remove ourselves
+                    if (!UpdateObjectInZone(gameId, type))
                     {
-                        // If it was placed add it to the world and set the correct zone
-                        AddObjectToWorld(badgo, zone);
-                        _badObjects.Remove(e.Object.Id);
-                        badgo.zone = zone;
+                        // Object not in zone, remove this object
+                        GameManager.RemoveSpawnedObject(_gameObjects[uniqueId].Item1);
+                        _gameObjects.Remove(uniqueId);
                     }
                 }
-                return;
+                else
+                {
+                    var pos = ScreenToWorld(Position(screenObject1, screenObject2));
+                    // We need to spawn it
+                    var (canSpawn, zone) = CanSpawn(type, pos);
+                    if (canSpawn)
+                    {
+                        // if we can be spawned, spawn us within that zone
+                        var angle = Direction(screenObject1, screenObject2);
+                        var rot = Quaternion.AngleAxis(Mathf.Rad2Deg * angle, Vector3.up);
+                        var gameId = GameManager.SpawnPrefab(type.ToString(), pos, rot);
+                        _gameObjects.Add(uniqueId, (gameId, type));
+                        zone?.Place(gameId);
+                    }
+                }
             }
-            Debug.LogWarning("Tried to update an object that was not added before. Maybe you reached max objects?");
-            return;
-        }
-        var go = _gameObjects[e.Object.Id];
-        var gameId = go.gameId;
-        go.screenPosition = new Vector2(e.Object.X, 1 - e.Object.Y);
-        go.angle = e.Object.Angle;
-        GameManager.SetPositionOfSpawnedObject(gameId, ScreenToWorld(go.screenPosition));
-
-        var (placed, z) = TryPlace(go);
-        // If we are no longer within a correct zone, or the same zone turn the object into a bad object
-        if (!placed || z != go.zone)
-        {
-            GameManager.RemoveSpawnedObject(gameId);
-            _gameObjects.Remove(go.id);
-            _badObjects.Add(go.id, go);
-            go.gameId = 0;
-            go.zone = null;
-        }
-    }
-
-    private void OnObjectRemoved(object sender, TuioObjectEventArgs e)
-    {
-        if (!_gameObjects.ContainsKey(e.Object.Id))
-        {
-            if (_badObjects.ContainsKey(e.Object.Id))
+            else
             {
-                _badObjects.Remove(e.Object.Id);
-                return;
+                // Otherwise we must remove any occurence of this object
+                if (_gameObjects.ContainsKey(uniqueId))
+                {
+                    GameManager.RemoveSpawnedObject(_gameObjects[uniqueId].Item1);
+                    _gameObjects.Remove(uniqueId);
+                }
             }
-            Debug.LogError("Tried to remove an object that was not added priorly.");
-            return;
         }
-
-        GameManager.RemoveSpawnedObject(_gameObjects[e.Object.Id].gameId);
-        _gameObjects.Remove(e.Object.Id);
     }
 
     private Vector3 ScreenToWorld(Vector2 pos)
@@ -221,54 +119,61 @@ public class ObjectManager : MonoBehaviour
         return Camera.main.ViewportToWorldPoint(new Vector3(x,  y, Camera.main.transform.position.y)); // y = 0 is our playing field plane
     }
 
-    private void AddObjectToWorld(TUIOObject obj, Zone zone)
+    private Vector2 Position(ScreenObject obj1, ScreenObject obj2)
     {
-        var rot = Quaternion.AngleAxis(Mathf.Rad2Deg * obj.angle, Vector3.up);
-        var gameId = GameManager.SpawnPrefab(obj.type.ToString(), ScreenToWorld(obj.screenPosition), rot);
-        obj.gameId = gameId;
-        _gameObjects.Add(obj.id, obj);
-        zone?.OnEnter(GameManager.GetSpawnedObject(obj.gameId));
+        return (obj1.screenPosition + obj2.screenPosition) * .5f;
     }
-    
-    private (bool, Zone) TryPlace(TUIOObject obj)
+
+    /**
+     * Returns the angle of the direction from pos obj1 to pos obj2
+     */
+    private float Direction(ScreenObject obj1, ScreenObject obj2)
     {
-        var worldPos = ScreenToWorld(obj.screenPosition);
-        var zonesOfType = zones.Where(zone => zone.Type == obj.type);
+        var delta = obj2.screenPosition - obj1.screenPosition;
+        return Mathf.Atan2(delta.x, delta.y);
+    }
 
-        if (!zonesOfType.Any())
-        {
-            // If there is no zone of this type, allow placement allover
-            return (true, null);
-        }
+    private (bool, Zone) CanSpawn(ObjectType type, Vector3 pos)
+    {
+        var m = MaxObjectsPerType.FirstOrDefault(mopt => mopt.type == type);
 
-        var inside = false;
+        // No max amount => any amount
+        var zonesOfType = zones.Where(zone => zone.Type == type);
+        bool canSpawnZone = !zonesOfType.Any();
+
         Zone retZone = null;
-        foreach (var zone in zones)
+        foreach (var zone in zonesOfType)
         {
-            if (zone.Type != obj.type) continue;
+            if (!zone.CanBePlaced(pos)) continue;
 
-            // IsPlaceable needs to be called on every single zone, as it acts as an update as well
-            // However, if we can be placed in one zone, it means object can be placed
-            if (!zone.TryPlace(obj.id, worldPos)) continue;
-
-            inside = true;
+            canSpawnZone = true;
             retZone = zone;
         }
 
-        return (inside, retZone);
+        if (m == null)
+        {
+            return (false, null);
+        }
+
+        return CountSpawnedObjectsOfType(type) < m.max ? (canSpawnZone, retZone) : (false, null);
     }
 
-    private bool MaxObjectsReached(Objects type)
+    private bool UpdateObjectInZone(long id, ObjectType type)
     {
-        var maxStruct = MaxObjectsPerType.FirstOrDefault(m => m.type == type);
-        return maxStruct == null || _gameObjects.Values.Count(g => g.type == type) >= maxStruct.max;
+        var objectZone = zones.Where(zone => zone.Type == type && zone.Contains(id));
+        foreach (var zone in objectZone)
+        {
+            return zone.Update(id);
+        }
+
+        return true;
     }
 
     public MaxObject[] GetMaxObjectsPerType () {
         return MaxObjectsPerType;
     }
 
-    public int CountSpawnedObjectsOfType(Objects type) {
-        return _gameObjects.Values.Count(g => g.type == type);
+    public int CountSpawnedObjectsOfType(ObjectType type) {
+        return _gameObjects.Count((obj) => obj.Value.Item2 == type);
     }
 }
