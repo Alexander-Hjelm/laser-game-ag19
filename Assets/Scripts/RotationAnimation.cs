4614a1faf1b1ec5526cdd04de69d403d7c7a3d97
agylling <agylling@kth.se>
2019-10-13 14:33:45 +0200

    Added Sparking Wires, and airvents

--- Assets/Scripts/RotationAnimation.cs
+++ Assets/Scripts/RotationAnimation.cs
@@ -8,6 +8,6 @@ public class RotationAnimation : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0f, _speed*Time.deltaTime, 0f, Space.World);
+        transform.Rotate(0f, _speed*Time.deltaTime, 0f, Space.Self);
     }
 }
