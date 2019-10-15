72440ae16e65951031e6833c1f0972776a53c36d
Alexander Hjelm <nick.nertova@gmail.com>
2019-10-15 18:24:52 +0200

    Glass walls

---
+++ Assets/Scripts/Objects/GlassWall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassWall : MonoBehaviour
+{
+    [SerializeField] private Color _color;
+
+    void Start()
+    {
+        // Material setup
+        Material newMat = GetComponent<MeshRenderer>().material;
+        newMat.color = new Color(_color.r, _color.g, _color.b, 0.5f);
+        GetComponent<MeshRenderer>().material = newMat;
+    }
+
+    public Color GetColor()
+    {
+        return _color;
+    }
+}
