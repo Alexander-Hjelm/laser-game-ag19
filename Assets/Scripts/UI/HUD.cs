715576e71c641b6344fe37ec01c5d5488326d58d
bhatnathan <bhatnathan@gmail.com>
2019-10-30 14:50:11 +0100

    Changed UI system

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -5,39 +5,41 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class HUD : MonoBehaviour
-{
+public class HUD : MonoBehaviour {
     private TextMeshProUGUI hudText;
     private ObjectManager objMan;
     private ObjectManager.MaxObject[] mo;
 
-    private string[] icons = {"<sprite=\"icons\" index=2>", "<sprite=\"icons\" index=0>", "<sprite=\"icons\" index=1>" };
+    private string[] icons = { "<sprite=\"icons\" index=2>", "<sprite=\"icons\" index=0>", "<sprite=\"icons\" index=1>" };
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         hudText = GetComponent<TextMeshProUGUI>();
         objMan = GameObject.FindGameObjectWithTag("GameController").GetComponent<ObjectManager>();
-        mo = objMan.GetMaxObjectsPerType ();
+        mo = objMan.GetMaxObjectsPerType();
 
         UpdateHUD();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        //UpdateHUD(); // TODO possibly move this to only be called when an object is added or removed
+    void Update() {
+        UpdateHUD(); // TODO possibly move this to only be called when an object is added or removed
     }
 
     // Counts current placed phycons and updates the HUD to display remaining objects
-    private void UpdateHUD () {
+    private void UpdateHUD() {
         StringBuilder sb = new StringBuilder("");
+        sb.Append("<font=\"sofachrome rg SDF\"> <mark=#41438080>");
         foreach (var cur in mo) {
-            sb.Append(icons[(int) cur.type]);
-            sb.Append(" X ");
+            sb.Append(icons[(int)cur.type]);
+            sb.Append(" : ");
+            sb.Append(objMan.CountSpawnedObjectsOfType(cur.type));
+            sb.Append("/");
             sb.Append(cur.max);
             sb.Append(" ");
         }
+        sb.Append("</font>");
+        sb.Append("</mark>");
         hudText.text = sb.ToString();
     }
 }
